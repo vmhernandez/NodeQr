e32a954483f47b16d5d413f33e1927d6b63c0830
Víctor Manuel Hernández Vergara <vmhernandez@ing.ucsc.cl>
2016-06-12 00:17:39 -0300

    Delete conexion.php

--- conexion.php
+++
@@ -1,6 +0,0 @@
-<?php
-    function conectarse(){ 
-        $link=mysql_connect("localhost","root","");
-        mysql_select_db("masisa",$link) OR DIE ("Error: No es posible establecer la conexión");
-    }
-?>
